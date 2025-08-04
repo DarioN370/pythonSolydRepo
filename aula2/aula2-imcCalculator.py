86ccf2e947016601cbb7bbc2b8f3fff913913d6b
DarioN370 <netobis2015@gmail.com>
2025-08-04 10:40:11 -0300

    work BMI Calculator, without if else

--- aula2/aula2-imcCalculator.py
+++ aula2/aula2-imcCalculator.py
@@ -6,4 +6,16 @@
 # print('your sum is: ' + str(add))
 #----------------------------------- IMC Calculator --------------------------------------
 
+weight = float(input('Type your weight in kg: '))
+height = float(input('Type your height in m: '))
+bmi = weight / (height * height)
 
+
+print('\n ⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘'
+    '\n Your BMI is: ' + str(bmi) +
+      '\n ⫘⫘⫘⫘⫘⫘⫘⫘⫘⫘'
+      '\n <18.5 = Underweight '
+      '\n 18.5 - 24.9 = Normal'
+      '\n 25 - 29.9 = Overweight'
+      '\n >= 30 = Obesive'
+      )
