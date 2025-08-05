cfb43721dd8fb4eae0c2ddd7c7c80d2aae7b8422
DarioN370 <netobis2015@gmail.com>
2025-08-05 14:29:55 -0300

    init class 5

---
+++ class5-while-for/while-for.py
@@ -0,0 +1,4 @@
+names = ['Guilherme', 'Dario', 'Marcelo', 'Jym', 'Sheldon']
+
+for name in names:
+    print(name) #That “name” can call anything, like game in games, car in cars, or number in numbers, just every create this making a sense.
