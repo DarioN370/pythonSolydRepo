c11905b5cc0c876f3c892b1d6e25441a5f60b19d
DarioN370 <netobis2015@gmail.com>
2025-08-06 10:58:03 -0300

    update class5

--- class5-while-for/while-for.py
+++ class5-while-for/while-for.py
@@ -1,4 +1,19 @@
 names = ['Guilherme', 'Dario', 'Marcelo', 'Jym', 'Sheldon']
 
-for name in names:
-    print(name) #That “name” can call anything, like game in games, car in cars, or number in numbers, just every create this making a sense.
+for i in names:
+    print(i) #That “name” can call anything, like game in games, car in cars, or number in numbers, just every create this making a sense.
+
+for i in range(5): #same thing.
+    print(names[i])
+
+for i in range(len(names)): #same thing - Aqui ele pega o tamanho da lista, e printa o tamanho certo caso eu não saiba o tamanho dela
+    print(names[i])
+
+###############################################################
+
+
+##################### OR CAN I USE THIS ###########################
+#list_numbers = range(5) - this is the same thing as this behind.
+number: int # Here im just add a type to VAR because python as rage me.
+for number in range(5): #way to make a list more quickly
+    print(number)
