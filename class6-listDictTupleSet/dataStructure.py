a10e9277118c905f2d49af56790e755075d90db2
DarioN370 <netobis2015@gmail.com>
2025-10-04 19:08:48 -0300

    init class 6 - list, dict, tuples ...

--- class6-listDictTupleSet/dataStructure.py
+++ class6-listDictTupleSet/dataStructure.py
@@ -1,1 +1,12 @@
-minha_lista = ['A', 'B', 'C', 'D', 'E', 'F'];
+my_list = ['A', 'B', 'C', 'D', 'E', 'F'] #LISTA muda
+my_tuple = ('Gui', 'Joao') #imutavel
+my_dict = {'nome': 'Dario', 'idade': 22} #funciona como um dictionary real
+my_set = {'Gui', 'Pedro', 'Carr'} #CONJUNTO - no repeat, mesmo que coloque dois iguais, ele vai contar como se tivesse um, alem dele não ter ordem, ou seja, não tem indice, não tem como voce buscar o item numero 0 do conjunto, ou o indice 3
+
+#TUPLE
+print(my_tuple)
+print(type(my_tuple))
+print(my_tuple[1] + ", esse é o nome no indice 1")
+for name in my_tuple:
+    print (name)
+# my_tuple[0] = 'joão' - isso daria erro, a tupla é imutavel lembra ?
